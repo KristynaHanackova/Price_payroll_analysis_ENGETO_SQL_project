57efbb3b8e3e4714363e5d0fd184919acc51f699
KristynaHanackova <126605669+KristynaHanackova@users.noreply.github.com>
2023-04-27 15:57:10 +0200

    Update Question_no_05.sql

--- Question_no_05.sql
+++ Question_no_05.sql
@@ -2,7 +2,7 @@ WITH tabulka AS (
 SELECT 
 	tabs.country, 
 	tabs.`year`, 
-	round(tabs.GDP, 0) AS GDP,
+	ROUND(tabs.GDP, 0) AS GDP,
 	tabp.payroll_year, 
 	ROUND(AVG(tabp.AVG_payroll), 2) AS payroll,
 	ROUND(AVG (tabp.AVG_price), 2) AS price
@@ -12,8 +12,8 @@ WHERE tabs.country LIKE '%czech%'
 GROUP BY tabp.payroll_year 
 )
 SELECT tab.country, tab.YEAR, tab. GDP, tab.payroll, tab.price,
-	round((tab.GDP - tab2.GDP) / tab2.GDP * 100, 2) AS GDP_annual_change,
-	round((tab.payroll - tab2.payroll) / tab2.payroll * 100, 2) AS payroll_annual_change,
-	round((tab.price - tab2.price) / tab2.price * 100, 2) AS price_annual_change
+	ROUND((tab.GDP - tab2.GDP) / tab2.GDP * 100, 2) AS GDP_annual_change,
+	ROUND((tab.payroll - tab2.payroll) / tab2.payroll * 100, 2) AS payroll_annual_change,
+	ROUND((tab.price - tab2.price) / tab2.price * 100, 2) AS price_annual_change
 FROM tabulka tab
 	JOIN tabulka tab2 ON tab.YEAR = tab2.YEAR + 1
