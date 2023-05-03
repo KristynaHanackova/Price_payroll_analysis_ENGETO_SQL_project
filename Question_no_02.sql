2b86819c16b1ba8338c202e3c3cf3d685c41c3a8
KristynaHanackova <126605669+KristynaHanackova@users.noreply.github.com>
2023-05-03 12:19:43 +0200

    Update Question_no_02.sql
    Calculated average and round in lines 5 and 6

--- Question_no_02.sql
+++ Question_no_02.sql
@@ -2,8 +2,8 @@ WITH tabulka AS (
 SELECT 
 	name,
 	payroll_year AS year, 
-	AVG_payroll, 
-	AVG_price 
+	ROUND(AVG(AVG_payroll),0) as AVG_payroll, 
+	ROUND(AVG(AVG_price),2) as AVG_price 
 FROM t_kristyna_hanackova_project_sql_primary_final tkhpspf
 WHERE payroll_year = 2006 OR payroll_year = 2018 
 GROUP BY payroll_year, name 
